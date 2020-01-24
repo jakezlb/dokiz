b7fa6a44a84ea3f4e67e63f1e31966595b5f4ec5
Jakez <lebarsjakez@gmail.com>
2020-01-24 11:59:17 +0100

    secutiry advanced -> 3a on symfony

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'the_new_password'
+        ));
+    }
+}
