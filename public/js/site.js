1885cc3f24b297e0d5da9683383f8a89e96c0fe4
Jakez <lebarsjakez@gmail.com>
2020-02-28 15:20:55 +0100

    add top on main and return on profile

--- public/js/site.js
+++ public/js/site.js
@@ -10,6 +10,8 @@ $(function () {
         section2.toggleClass('btnNavActive', $(this).scrollTop() > (navTriggerHeight2 * 120) / 100 && $(this).scrollTop() < (navTriggerHeight2*230) / 100);
         section3.toggleClass('btnNavActive', $(this).scrollTop() > (navTriggerHeight2 * 230) / 100 && $(this).scrollTop() < (navTriggerHeight2*280) / 100);
         section4.toggleClass('btnNavActive', $(this).scrollTop() > (navTriggerHeight2 * 280) / 100);
+
+        $('#topSection').scrollTop(0);
     });
 
     $('#username').hover(function() {
