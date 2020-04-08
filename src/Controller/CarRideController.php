85e45a8547d0ba7a210eea11ca2cd992134d5ad9
GeoffrayBigot <drannox.g@gmail.com>
2020-04-08 14:46:59 +0200

    Relations pour les trajets et les reservations

--- src/Controller/CarRideController.php
+++ src/Controller/CarRideController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/car/ride")
+ * @Route("/car_ride")
  */
 class CarRideController extends AbstractController
 {
