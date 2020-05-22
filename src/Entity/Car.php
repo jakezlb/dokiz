36d61fbdd339a43c5ca11676b63726f3e9427658
Jakez Le Bars <jakez.lebars@kepler-solutions.fr>
2020-05-22 14:09:53 +0200

    fix bug

--- src/Entity/Car.php
+++ src/Entity/Car.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CarRepository")
