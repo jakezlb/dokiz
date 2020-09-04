cc84ab035ed64d907bc4b9eca8c5aa904fae07a8
AdelineLemoussu <adeline.lemoussu@neuf.fr>
2020-09-04 18:49:46 +0200

    edit error same amil bdd

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 
 class RegisterController extends AbstractController
 {
@@ -21,32 +22,44 @@ class RegisterController extends AbstractController
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
+        
+        
+            if($form->isSubmitted() && $form->isValid()) {
+                $data = $form->getData();
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
+                $password = $data->getPassword();
+                $user->setPassword(
+                    $passwordEncoder->encodePassword($user, $password)
+                );
 
-            $password = $data->getPassword();
-            $user->setPassword(
-                $passwordEncoder->encodePassword($user, $password)
-            );
+                $user->setRoles(['ROLE_USER']);
+                $user->setCreatedAt(new \DateTime());
+                try{
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($user);
+                    $em->flush();
+                    $this->addFlash('Success', 'Votre compte a bien été crée'); 
 
-            $user->setRoles(['ROLE_USER']);
-            $user->setCreatedAt(new \DateTime());
+                    $email = (new Email())
+                    ->from('lebarsjakez@gmail.com')
+                    ->to($user->getEmail())
+                    ->subject('Bienvenue chez Dokiz !')
+                    ->text("Bienvenue chez Dokiz " . $user->getFirstName() ." ". $user->getLastName());
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+                    $mailer->send($email);
+                    
+                    return $this->redirect($this->generateUrl('app_login'));
 
-            $email = (new Email())
-                ->from('lebarsjakez@gmail.com')
-                ->to($user->getEmail())
-                ->subject('Bienvenue chez Dokiz !')
-                ->text("Bienvenue chez Dokiz " . $user->getFirstName() ." ". $user->getLastName());
+                } catch(UniqueConstraintViolationException $e){
 
-            $mailer->send($email);
+                    $this->addFlash('danger', 'L\'email est déjà existant');  
+                    return $this->redirectToRoute('register');
+                    
+                }
 
-            return $this->redirect($this->generateUrl('app_login'));
-        }
+                
+            }
+        
 
         return $this->render('register/index.html.twig', [
             'form' => $form->createView()
